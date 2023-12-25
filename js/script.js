b622fec7aa49c2dfb5d6bf93e7117d49ad8d3e0b
Naim <143653983+naim3450@users.noreply.github.com>
2023-12-25 07:43:22 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+// 1st way
+
+// let naim = {
+//     'name': 'naim',
+//     'age': 18,
+//     'height': 'six-fut',
+// }
+// console.log(naim)
+
+
+// 2nd way
+// let naim = {
+//     name: 'naim',
+//     age: 18,
+//     height: 'six-fut',
+// }
+// let saim = naim
+// console.log(saim)
+
+// 3rd way
+// let naim = {
+//     name: 'naim',
+//     age: 18,
+//     height: 'six-fut',
+// }
+// let saim ={
+//     name: naim.name,
+//     age: naim.age,
+//     height: naim.height,
+//     bari: 'bahara'
+// }
+
+// console.log(saim)
+
+
+// 4th way
+// let naim ={
+//     name: "naim",
+//     age: 18,
+//     height: "six-fut"
+// }
+
+// let saim = {...naim, area: "mirpur-10"}
+// console.log(saim)
+
+
+// split
+
+let naim = "naim saim sakib roney arman jarif sajjat azim "
+let saim = naim.split.("")
+console.log(saim)
